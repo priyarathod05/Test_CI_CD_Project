adaca0af506317d58025f576ccb986a3ce657ea1
Priyarathod <rathodpriya.0501@gmail.com>
2024-12-27 10:47:50 +0530

    Test

---
+++ my_fist_test_project.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);        // Enable error reporting
+ini_set('display_errors', 1);  // Display errors
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page
+
+<?php
+echo "Hello World!";?>
+
+
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '91dc33da273487761cab7b1fee64b49ae93f677f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '91dc33da273487761cab7b1fee64b49ae93f677f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
